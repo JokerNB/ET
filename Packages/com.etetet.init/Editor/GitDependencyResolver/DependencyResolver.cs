399c799ac175dfc659a28f3eaa0efd202328d37d
lw <17639892535@163.com>
2025-12-03 14:54:46 +0800

    忽略Unity6版本判断
    1.取消版本判断是packagecache移动包出错
    2.取消Bundle、/ET.sln、奥丁工具的忽略

--- Packages/com.etetet.init/Editor/GitDependencyResolver/DependencyResolver.cs
+++ Packages/com.etetet.init/Editor/GitDependencyResolver/DependencyResolver.cs
@@ -22,13 +22,13 @@ namespace Hibzz.DependencyResolver
         {
             string packageName = default;
             string moveFileName = default;
-            #if UNITY_6000_0_OR_NEWER
-            packageName = package;
-            moveFileName = "MoveToPackages_6";
-            #else 
+            // #if UNITY_6000_0_OR_NEWER
+            // packageName = package;
+            // moveFileName = "MoveToPackages_6";
+            // #else 
             packageName =$"{package}@{version}";
             moveFileName = "MoveToPackages";
-            #endif
+            // #endif
             string dir = Path.Combine("Library/PackageCache", packageName);
             if (!Directory.Exists(dir))
             {
@@ -92,11 +92,11 @@ namespace Hibzz.DependencyResolver
                 
                 string[] ss = baseName.Split("@");
                 string packageName = ss[0];
-                #if UNITY_6000_0_OR_NEWER
-                string version = "";
-                #else 
+                // #if UNITY_6000_0_OR_NEWER
+                // string version = "";
+                // #else 
                 string version = ss[1];
-                #endif
+                // #endif
 
                 MoveToPackage(packageName, version);
             }
