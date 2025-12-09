52f7e7ff32d5fd2469545405c1e865e9e94f4613
Lw <17639892535@163.com>
2025-12-09 22:23:52 +0800

    添加EnableClass特性忽略目录

--- Packages/cn.etetet.sourcegenerator/DotNet~/ET.SourceGenerator/AnalyzerGlobalSetting.cs
+++ Packages/cn.etetet.sourcegenerator/DotNet~/ET.SourceGenerator/AnalyzerGlobalSetting.cs
@@ -1,4 +1,6 @@
-﻿namespace ET
+﻿using System.Collections.Generic;
+
+namespace ET
 {
     public static class AnalyzerGlobalSetting
     {
@@ -6,5 +8,13 @@
         /// 是否开启项目的所有分析器
         /// </summary>
         public static bool EnableAnalyzer = true;
+
+        /// <summary>
+        /// EnableClass特性忽略目录
+        /// </summary>
+        public static HashSet<string> EnableClassIgnoreDirNames = new HashSet<string>()
+        {
+
+        };
     }
 }
