a6b64270f30ae5e44d5bdb4c732339ca42273ccc
Lw <17639892535@163.com>
2025-12-10 01:14:24 +0800

    添加静态字段忽略文件夹逻辑

--- Packages/cn.etetet.sourcegenerator/DotNet~/ET.SourceGenerator/Analyzer/StaticFieldDeclarationAnalyzer.cs
+++ Packages/cn.etetet.sourcegenerator/DotNet~/ET.SourceGenerator/Analyzer/StaticFieldDeclarationAnalyzer.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Immutable;
+using System.IO;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
@@ -34,6 +36,42 @@ namespace ET
             {
                 return;
             }
+            
+            var symbol = context.Symbol;
+        
+            // 获取符号的所有声明位置
+            foreach (var location in symbol.Locations)
+            {
+                if (location.IsInSource)
+                {
+                    // 获取SyntaxTree和文件路径
+                    var syntaxTree = location.SourceTree;
+                    var filePath = syntaxTree.FilePath;
+                    
+                    //针对配置相对目录做屏蔽
+                    filePath = filePath.Replace('\\', '/').Replace("//", "/");
+                    if (AnalyzerGlobalSetting.EnableClassIgnoreDirNames.Any(dir => filePath.Contains(dir.Replace('\\', '/').Replace("//", "/"))))
+                    {
+                        return;
+                    }
+                
+                    // // 获取行号信息
+                    // var lineSpan = location.GetLineSpan();
+                    // var lineNumber = lineSpan.StartLinePosition.Line + 1;
+                    // var columnNumber = lineSpan.StartLinePosition.Character + 1;
+                    //
+                    // // 获取完整路径或文件名
+                    // var fileName = Path.GetFileName(filePath);
+                    // var directory = Path.GetDirectoryName(filePath);
+                    
+                
+                    // // 使用文件路径
+                    // Console.WriteLine($"File: {filePath}");
+                    // Console.WriteLine($"Location: Line {lineNumber}, Column {columnNumber}");
+                }
+            }
+            
+
 
             foreach (ISymbol? memberSymbol in namedTypeSymbol.GetMembers())
             {
